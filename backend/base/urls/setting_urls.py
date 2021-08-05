aaaf77a2f8723d805a8ec6a11ef770087d5ccb3e
Ji-Yang Lin <xuunnis123@gmail.com>
2021-08-05 13:53:24 +0800

    add setting endpoints

--- backend/base/urls/setting_urls.py
+++ backend/base/urls/setting_urls.py
@@ -4,8 +4,29 @@ from base.views import setting_views as views
 urlpatterns =[
   
     path('income/contributeContextList', views.getIncomeContributeContextList, name="get_income_contribute_contextList"),
+    path('income/contributeContext/<str:pk>', views.getIncomeContributeContext, name="get_income_contribute_context"),
+    path('income/contributeContext/create/', views.addIncomeContributeContext, name="add_income_contribute_context"),
+    path('income/contributeContext/update/<str:pk>', views.updateIncomeContributeContext, name="update_income_contribute_context"),
+    path('income/contributeContext/delete/<str:pk>', views.deleteIncomeContributeContext, name="delete_income_contribute_context"),
+    
     path('outcome/contributeContextList', views.getOutcomeContributeContextList, name="get_outcome_contribute_contextList"),
+    path('outcome/contributeContext/<str:pk>', views.getOutcomeContributeContext, name="get_outcome_contribute_context"),
+    path('outcome/contributeContext/create/', views.addOutcomeContributeContext, name="add_outcome_contribute_context"),
+    path('outcome/contributeContext/update/<str:pk>', views.updateOutcomeContributeContext, name="update_outcome_contribute_context"),
+    path('outcome/contributeContext/delete/<str:pk>', views.deleteOutcomeContributeContext, name="delete_outcome_contribute_context"),
+    
     path('income/moneycategoryList', views.getIncomeMoneyCategoryList, name="get_income_money_categoryList"),
+    path('income/moneycategory/<str:pk>', views.getIncomeMoneyCategory, name="get_income_moneycategory"),
+    path('income/moneycategory/create/', views.addIncomeMoneyCategory, name="add_income_moneycategory"),
+    path('income/moneycategory/update/<str:pk>', views.updateIncomeMoneyCategory, name="update_income_moneycategory"),
+    path('income/moneycategory/delete/<str:pk>', views.deleteIncomeMoneyCategory, name="delete_income_moneycategory"),
+    
     path('outcome/moneycategoryList', views.getOutcomeMoneyCategoryList, name="get_outcome_money_categoryList"),
-   
+    path('outcome/moneycategory/<str:pk>', views.getOutcomeMoneyCategory, name="get_outcome_moneycategory"),
+    path('outcome/moneycategory/create/', views.addOutcomeMoneyCategory, name="add_outcome_moneycategory"),
+    path('outcome/moneycategory/update/<str:pk>', views.updateOutcomeMoneyCategory, name="update_outcome_moneycategory"),
+    path('outcome/moneycategory/delete/<str:pk>', views.deleteOutcomeMoneyCategory, name="delete_outcome_moneycategory"),
+
+
+
 ]
--- backend/base/views/setting_views.py
+++ backend/base/views/setting_views.py
@@ -19,10 +19,101 @@ def getIncomeContributeContextList(request):
     return Response(serializer.data)
 
 @api_view(['GET'])
+def getIncomeContributeContext(request,pk):
+    incomeContributeContext = IncomeContributeContext.objects.get(_id=pk)
+    serializer = IncomeContributeContextSerilizer(incomeContributeContext, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addIncomeContributeContext(request):
+    
+    data = request.data
+    
+    if data and len(data) == 0:
+        return Response({'detail': 'No Income Contribute Context Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        
+        incomeContributeContext = IncomeContributeContext.objects.create(
+            context = data['context'],
+        )
+
+        serializer = IncomeContributeContextSerilizer(incomeContributeContext, many=False)
+        print("serializer:",serializer)
+        return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateIncomeContributeContext(request,pk):
+    incomeContributeContext = IncomeContributeContext.objects.get(_id=pk)
+    if incomeContributeContext:
+        data = request.data
+        if data.get('context'):
+            incomeContributeContext.context = data['context']
+    incomeContributeContext.save()
+    serializer = IncomeContributeContextSerilizer(incomeContributeContext,many=False)
+    
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteIncomeContributeContext(request, pk):
+    incomeContributeContextForDeletion = IncomeContributeContext.objects.get(_id=pk)
+    incomeContributeContextForDeletion.delete()
+    return Response('此收入項目已刪除')
+
+@api_view(['GET'])
 def getOutcomeContributeContextList(request):
     outcomeContributeContextList = OutcomeContributeContext.objects.all()
     serializer = OutcomeContributeContextSerilizer(outcomeContributeContextList, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getOutcomeContributeContext(request,pk):
+    outcomeContributeContext = OutcomeContributeContext.objects.get(_id=pk)
+    serializer = OutcomeContributeContextSerilizer(outcomeContributeContext, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOutcomeContributeContext(request):
+    
+    data = request.data
+    
+    if data and len(data) == 0:
+        return Response({'detail': 'No Outcome Contribute Context Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        
+        outcomeContributeContext = OutcomeContributeContext.objects.create(
+            context = data['context'],
+        )
+
+        serializer = OutcomeContributeContextSerilizer(outcomeContributeContext, many=False)
+        print("serializer:",serializer)
+        return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateOutcomeContributeContext(request,pk):
+    outcomeContributeContext = OutcomeContributeContext.objects.get(_id=pk)
+    if outcomeContributeContext:
+        data = request.data
+        if data.get('context'):
+            outcomeContributeContext.context = data['context']
+    outcomeContributeContext.save()
+    serializer = OutcomeContributeContextSerilizer(outcomeContributeContext,many=False)
+    
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteOutcomeContributeContext(request, pk):
+    outcomeContributeContextForDeletion = OutcomeContributeContext.objects.get(_id=pk)
+    outcomeContributeContextForDeletion.delete()
+    return Response('此收入項目已刪除')
+
 
 @api_view(['GET'])
 def getIncomeMoneyCategoryList(request):
@@ -31,7 +122,103 @@ def getIncomeMoneyCategoryList(request):
     return Response(serializer.data)
 
 @api_view(['GET'])
+def getIncomeMoneyCategory(request,pk):
+    incomeMoneyCategory = IncomeMoneyCategory.objects.get(_id=pk)
+    serializer = IncomeMoneyCategorySerilizer(incomeMoneyCategory, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addIncomeMoneyCategory(request):
+    
+    data = request.data
+    
+    if data and len(data) == 0:
+        return Response({'detail': 'No Income Money Category Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        
+        incomeMoneyCategory = IncomeMoneyCategory.objects.create(
+            name = data['name'],
+            detail = data['detail'],
+        )
+
+        serializer = IncomeMoneyCategorySerilizer(incomeMoneyCategory, many=False)
+        print("serializer:",serializer)
+        return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateIncomeMoneyCategory(request,pk):
+    incomeMoneyCategory = IncomeMoneyCategory.objects.get(_id=pk)
+    if incomeMoneyCategory:
+        data = request.data
+        if data.get('name'):
+            incomeMoneyCategory.name = data['name']
+        if data.get('detail'):
+            incomeMoneyCategory.detail = data['detail']
+    incomeMoneyCategory.save()
+    serializer = IncomeMoneyCategorySerilizer(incomeMoneyCategory,many=False)
+    
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteIncomeMoneyCategory(request, pk):
+    incomeMoneyCategoryForDeletion = IncomeMoneyCategory.objects.get(_id=pk)
+    incomeMoneyCategoryForDeletion.delete()
+    return Response('此收入單據類型已刪除')
+
+
+
+@api_view(['GET'])
 def getOutcomeMoneyCategoryList(request):
     outcomeMoneyCategoryList = OutcomeMoneyCategory.objects.all()
     serializer = OutcomeMoneyCategorySerilizer(outcomeMoneyCategoryList, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def getOutcomeMoneyCategory(request,pk):
+    outcomeMoneyCategory = OutcomeMoneyCategory.objects.get(_id=pk)
+    serializer = OutcomeMoneyCategorySerilizer(outcomeMoneyCategory, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOutcomeMoneyCategory(request):
+    
+    data = request.data
+    
+    if data and len(data) == 0:
+        return Response({'detail': 'No Outcome Money Category Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        
+        outcomeMoneyCategory = OutcomeMoneyCategory.objects.create(
+            name = data['name'],
+            datail = data['datail'],
+        )
+
+        serializer = OutcomeMoneyCategorySerilizer(outcomeMoneyCategory, many=False)
+        print("serializer:",serializer)
+        return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateOutcomeMoneyCategory(request,pk):
+    outcomeMoneyCategory = OutcomeMoneyCategory.objects.get(_id=pk)
+    if outcomeMoneyCategory:
+        data = request.data
+        if data.get('name'):
+            outcomeMoneyCategory.name = data['name']
+        if data.get('detail'):
+            outcomeMoneyCategory.detail = data['detail']
+    outcomeMoneyCategory.save()
+    serializer = OutcomeMoneyCategorySerilizer(outcomeMoneyCategory,many=False)
+    
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteOutcomeMoneyCategory(request, pk):
+    outcomeMoneyCategoryForDeletion = OutcomeMoneyCategory.objects.get(_id=pk)
+    outcomeMoneyCategoryForDeletion.delete()
+    return Response('此支出單據類型已刪除')
