04549f72c4d3a2d77f53a641c025370355a611a3
Ji-Yang Lin <xuunnis123@gmail.com>
2022-03-09 10:38:32 +0800

    update drive_api method

--- backend/drive_api.py
+++ backend/drive_api.py
@@ -8,9 +8,53 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+import argparse
+import json
+import os,io
+import os.path as path
+import re
+from apiclient import discovery
+from apiclient.http import MediaFileUpload, MediaIoBaseDownload
+
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
+
+drive_service = discovery.build('drive', 'v3', http=http)
+
+def listFiles(size):
+    results = drive_service.files().list(
+        pageSize=size,fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print('{0} ({1})'.format(item['name'], item['id']))
+
+def upload():
+    #TODO
+    file_metadata = {'name': 'photo.jpg'}
+    media = MediaFileUpload('files/photo.jpg', mimetype='image/jpeg')
+   
+    file = drive_service.files().create(body=file_metadata,
+                                    media_body=media,
+                                    fields='id').execute()
+    print('File ID: %s' % file.get('id'))
+
+def downloadFile(file_id,filepath):
+    request = drive_service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+    with io.open(filepath,'wb') as f:
+        fh.seek(0)
+        f.write(fh.read())
 
 def main():
     """Shows basic usage of the Drive v3 API.
